2413e2c5e04bca03bdfe75969f0be1f7dd87231b
ahmad-albaaj <ahmedb3aj@yahoo.com>
2021-05-25 02:40:45 +0000

    Modified

--- ui.R
+++ ui.R
@@ -6,9 +6,6 @@ ui <- fluidPage(
       sidebarLayout(
       sidebarPanel(selectInput("select", "Choose one:", 
                                choices = list("None" = 0,
-                                              "Pic 1" = 1, 
-                                              "Pic 2" = 2, 
-                                              "Pic 3" = 3)),
       textOutput("display"),
       sliderInput("slider1", label = h3("Image height"), 
                                         min = 1, max = 1000, value = 500)
